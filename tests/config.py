727a69b7bd510e82ee72d50ceec3e26c6251f6ef
wyattferguson <wyattf@gmail.com>
2025-06-11 14:20:13 -0400

    testing required and optional files

--- tests/config.py
+++ tests/config.py
@@ -1,6 +1,7 @@
 from random import randint
+from typing import Any
 
-COOKIE_FULL_BAKE: dict[str, str] = {
+cookie_full_bake: dict[str, Any] = {
     "project_name": f"test_full_bake ID{randint(1000, 9999)}",
     "project_slug": "test_full_bake",
     "project_description": "A great project that does cool things.",
@@ -18,9 +19,10 @@ COOKIE_FULL_BAKE: dict[str, str] = {
     "include_code_of_conduct": "y",
     "include_docker": "y",
     "pypi_deploy": "y",
+    "excluded_files": [],
 }
 
-COOKIE_MIN_BAKE: dict[str, str] = {
+cookie_min_bake: dict[str, Any] = {
     "project_name": f"Test Minimum Bake ID{randint(1000, 9999)}",
     "project_slug": "test_min_bake",
     "project_description": "A great project that does cool things.",
@@ -38,4 +40,25 @@ COOKIE_MIN_BAKE: dict[str, str] = {
     "include_code_of_conduct": "n",
     "include_docker": "n",
     "pypi_deploy": "n",
+    "excluded_files": [
+        "docs",
+        "noxfile.py",
+        "CHANGELOG.md",
+        "CONTRIBUTING.md",
+        "CODE_OF_CONDUCT.md",
+        "Dockerfile",
+        ".dockerignore",
+        ".github/workflows/pypi-deploy.yml",
+    ],
 }
+
+required_files: list[str] = [
+    "pyproject.toml",
+    "README.md",
+    "src",
+    "LICENSE",
+    ".vscode/settings.json",
+    ".vscode/extensions.json",
+    ".gitignore",
+    ".pre-commit-hooks.yaml",
+]
--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -8,9 +8,40 @@ import toml
 from pytest_cookies.plugin import Cookies, Result  # type: ignore[import-untyped]
 
 from tests.config import (
-    COOKIE_FULL_BAKE,
-    COOKIE_MIN_BAKE,
+    cookie_full_bake,
+    cookie_min_bake,
+    required_files,
 )
+
+
+@pytest.mark.parametrize("recipe", [cookie_full_bake, cookie_min_bake])
+def test_cookiecutter(cookies: Cookies, recipe: dict[str, Any]) -> None:
+    """Test the baking process with different recipes."""
+    project_path = bake_recipe(cookies, recipe)
+    pyproject = load_pyproject_toml(project_path)
+    verify_pyproject_bake(pyproject, recipe)
+    verify_excluded_files(project_path, recipe["excluded_files"])
+    verify_required_files(project_path, recipe["project_slug"])
+
+
+def verify_excluded_files(project_path: Path, files: list[str]) -> None:
+    """Verify that the given files do not exist in the project path."""
+    for file in files:
+        file_path = project_path / file
+        if file_path.exists():
+            pytest.fail(f"File should not exist: {file_path}")
+
+
+def verify_required_files(project_path: Path, project_slug: str) -> None:
+    """Verify that the given files exist in the project path."""
+    project_files: list[str] = [
+        f"src/{project_slug}",
+        f"src/{project_slug}/{project_slug}.py",
+    ] + required_files
+    for file in project_files:
+        file_path = project_path / file
+        if not file_path.exists():
+            pytest.fail(f"File should exist: {file_path}")
 
 
 def load_pyproject_toml(tmp_path: Path) -> dict[str, Any]:
@@ -25,7 +56,7 @@ def load_pyproject_toml(tmp_path: Path) -> dict[str, Any]:
             pytest.fail(f"Invalid TOML syntax in pyproject.toml: {e}")
 
 
-def verify_pyproject_bake(pyproject: dict[str, Any], recipe: dict[str, str]) -> None:
+def verify_pyproject_bake(pyproject: dict[str, Any], recipe: dict[str, Any]) -> None:
     """Verify the contents of the pyproject.toml file after baking."""
     if pyproject["project"]["name"] != recipe["project_slug"]:
         pytest.fail(f"Pyproject Error: {recipe['project_slug']} != {pyproject['project']['name']}")
@@ -40,20 +71,12 @@ def verify_pyproject_bake(pyproject: dict[str, Any], recipe: dict[str, str]) -> 
         )
 
 
-@pytest.mark.parametrize("recipe", [COOKIE_FULL_BAKE, COOKIE_MIN_BAKE])
-def test_cookiecutter(cookies: Cookies, recipe: dict[str, str]) -> None:
-    """Test the baking process with different recipes."""
-    project_path = bake_recipe(cookies, recipe)
-    pyproject = load_pyproject_toml(project_path)
-    verify_pyproject_bake(pyproject, recipe)
-
-
-def bake_recipe(cookies: Cookies, recipe: dict[str, str]) -> Path:
+def bake_recipe(cookies: Cookies, recipe: dict[str, Any]) -> Path:
     """Bake a new project from a cookiecutter template.
 
     Args:
         cookies (Cookies): The pytest-cookies fixture for baking.
-        recipe (dict[str, str]): The recipe containing project configuration.
+        recipe (dict[str, Any]): The recipe containing project configuration.
 
     Returns:
         Path: The path to the baked project.
